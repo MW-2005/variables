55b4ee9aae37b3ff7a6560ace6bfef86bbb7e2c1
Mya Walker <86728946+MW-2005@users.noreply.github.com>
2021-07-02 17:30:50 +0000

    answers

--- variables_primary.py
+++ variables_primary.py
@@ -1,7 +1,7 @@
-# author: <name here>
-# date: <date here>
+# author: <Mya Walker>
+# date: <7/2/21>
 #
-# description: <fill in>
+# description: <variable stuff???>
 
 # --------------- Section 1 --------------- #
 
@@ -20,11 +20,19 @@
 #   4) Print each variable, describing it when you print it.
 #
 # Example Code
-example_name = 'elia'
-print('EXAMPLE: my name is', example_name)
+    #example_name = 'elia'
+    #print('EXAMPLE: my name is', example_name)
 
 # WRITE CODE BELOW
+name='Mya'
+bday='6/10/05'
+animal= 'Bunnie'
 
+print()
+print('my name is', name)
+print('my bday is on', bday)
+print('my favorite animal at the moment is a', animal,'its always changing')
+print()
 
 
 # 1.2 | Variable Creation | Integers / Floats
@@ -46,6 +54,19 @@ print('EXAMPLE: my name is', example_name)
 
 # WRITE CODE BELOW
 
+num = '∞'
+
+numb= 3.14
+month= 'June'
+neg= "-1"
+deci= (0.5)
+
+print()
+print('my favorite number is', numb, 'commonly known as py')
+print('the glorious mounth on which I was born is', month,'the best month')
+print('this is a example of a negative number',neg)
+print('half can be shown in many forms such as',deci,'or 2/4')
+print()
 
 
 # 1.3 | Overwriting Variables
@@ -63,11 +84,20 @@ print('EXAMPLE: my name is', example_name)
 #   4) Print the variables you've overwritten, describing the values you print.
 #
 # Example Code
-example_name = 'lucia'
-print('EXAMPLE: my new name is', example_name)
+#example_name = 'lucia'
+#print('EXAMPLE: my new name is', example_name)
 
 # WRITE CODE BELOW
 
+name='Nicole'
+bday= '6/10/68'
+numb='69'
+
+print()
+print('my middle name is',name, 'so my inicials are M.N.W.')
+print('i think it would be cool to be born on',bday,'cause then i would be old enough to remember the 80\'s and the grate movies and music that came out around then ')
+print('the number i think is the most innapropreate is',numb,'you know why' )
+print()
 
 
 # 1.4 | Operations
@@ -80,7 +110,7 @@ print('EXAMPLE: my new name is', example_name)
 #   1) Create a variable that is the sum of two numbers.
 #   2) Create a variable that is the product of three numbers.
 #   3) Create a variable by dividing the previously created sum, with the previously created product.
-#
+
 #   4) Create a variable that is the concatenation of your name and an animal you like (use the variables!)
 #   5) Create a variable that is an acronym (like 'lol') multiplied by your birth day.
 #
@@ -91,7 +121,28 @@ print('EXAMPLE: my new name is', example_name)
 #   7) Print all the new variables you've created along with what the represent
 #
 # Example Code
-example_sum = 11 + 21
-print('EXAMPLE: the sum of 11 and 21 is', example_sum)
+    #example_sum = 11 + 21
+    #print('EXAMPLE: the sum of 11 and 21 is', example_sum)
 
 # WRITE CODE BELOW
+
+
+x=3+2
+num1=3*2*1
+num2=x/num1
+acro= 'WEML '*10
+y=4
+y-=3
+
+#i got confuse
+
+print()
+print(x)
+print(num1)
+print(num2)
+print(name+animal)
+print(acro)
+print(y)
+print()
+print()
+print()
